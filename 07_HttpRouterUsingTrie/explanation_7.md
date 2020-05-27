cba1fb39f187e457c263bf4449e0838e953519dd
Gregor Werum <gregor.werum@web.de>
2020-05-27 20:16:58 +0200

    Http router: add explanation and code comments

---
+++ 07_HttpRouterUsingTrie/explanation_7.md
@@ -0,0 +1,54 @@
+## Explanation for route_trie.py
+
+### Task
+
+The task of this exercise is to a implement a *Trie data structure* for storing all HTTPS request handlers of a homepage and its subpages. The focus is to implement the trie holding all routes (URL's) of the subpages, for the handlers simple strings as dummy structures will be used.
+
+### Code explanation
+
+Three classes have been implemented:
+
+#### class Router()
+Serves as interface class for handling requests. Has two public methods:
+1. **Router()::add_handler(*str handler, str homepage_path*)**: Adds new dummy handler for given homepage path and stores it in Trie.
+2. **Router()::lookup(*str homepage_path*)**: Returns handler for requested homepage path, if existing.
+
+#### class RouteTrie()
+Implementation of actual Trie data structure for storing routes and handlers. Has two public methods:
+1. **RouteTrie()::insert(*str handler, list[str] route_list*)**: adds new handler to Trie under given route list.
+2. **RouteTrie()::find(*list[str] route_list*)**: returns handler from given route list, if existing.
+
+#### class RouteTrieNode()
+Implements single node of route trie.
+
+---
+
+### Runtime efficiency
+
+Trie data structures have the advantage of having a very efficient lookup even if large amounts of data are stored in them. This is achieved by splitting up the lookup problem into smaller sub-problems. In this exercise homepage URL's are split up into smaller string sections, which are respectively stored in dictionaries. So, instead of looking up the entire URL in a large dictionary, each sections of the URL is looked up in a small dictionary. For small dictionaries the lookup can be performed in constant time O(1), so an URL split up into s sections can be looked up in O(s).
+
+|  | Time complexity | Space complexity |
+| ------------------- | --------------- | ---------------- |
+| Adding h handler with route size r | O(h*r) ~ **O(h)** | <= O(h*r) ~ **O(h)** |
+| Lookup handler for given route of size r | O(r) ~ **O(1)** | |
+
+**h**: total number of handlers stored in Trie\
+**r**: average size of URL route stored in Trie\
+
+For large homepages the number of handlers h is much larger then the average route r.
+
+---
+
+### Test Cases
+
+The following three test cases have been implemented to test the Router classes:
+
+1. **Test 1: Lookup of existing handlers**: Checks if existing handlers are correctly returned for given URL request.
+2. **Test 2: Lookup with trailing slash**: Checks if existing handlers are correctly returned when URL request contains trailing slash.
+3. **Test 3: Request for non-existing handlers**: Checks class behaviour for non-existing handlers.
+
+The test cases can be executed using the following command:
+
+```
+python -m unittest route_trie.py
+```
--- 07_HttpRouterUsingTrie/route_trie.py
+++ 07_HttpRouterUsingTrie/route_trie.py
@@ -1,11 +1,10 @@
 import unittest
 import pdb
-# A RouteTrieNode will be similar to our autocomplete TrieNode... with one additional element, a handler.
+
 class RouteTrieNode:
     def __init__(self, path, handler = None):
-        # Initialize the node with children as before, plus a handler
-        self.path = path
-        self.handler = handler
+        self.path = path    # Path section: /.../.../path/.../..
+        self.handler = handler 
         self.children = {}
 
     def __repr__(self, level=0):
@@ -15,79 +14,73 @@ class RouteTrieNode:
         return ret
 
     def insert(self, path):
-        # Insert the node as before
+        # Adds new child to node
         self.children[path] = RouteTrieNode(path)
 
-# A RouteTrie will store our routes and their associated handlers
 class RouteTrie:
+    # Stores all routes and handlers of the homepage
     def __init__(self, root_directory):
-        # Initialize the trie with an root node and a handler, this is the root path or home page node
+        # Homepage root holds main homepage URL
         self.root = RouteTrieNode(root_directory)
 
-    def insert(self, handler, path_list):
-        # Similar to our previous example you will want to recursively add nodes
-        # Make sure you assign the handler to only the leaf (deepest) node of this path
+    def insert(self, handler, route_list):
+        # Insert new handler for homepage path
         current_node = self.root
 
-        for path in path_list:
+        for path in route_list:
             if path not in current_node.children:
                 current_node.insert(path)
             current_node = current_node.children[path]
 
         current_node.handler = handler
 
-
-    def find(self, path_list):
-        # Starting at the root, navigate the Trie to find a match for this path
-        # Return the handler for a match, or None for no match
+    def find(self, route_list):
+        # Returns handler for given homepage path, if existing
         current_node = self.root
 
-        for path in path_list:
+        for path in route_list:
             children = current_node.children
             if path not in children:
-                return None
+                raise KeyError("No matching path found in Route Trie.")
             current_node = children[path]
 
         return current_node.handler
 
-# The Router class will wrap the Trie and handle 
 class Router:
+    # Wrapper class for route trie and handle setter and getter
     def __init__(self, root_directory):
-        # Create a new RouteTrie for holding our routes
-        # You could also add a handler for 404 page not found responses as well!
+        # Initialize new route trie
         self.route = RouteTrie(root_directory)
 
     def __repr__(self):
         return self.route.root.__repr__()
 
-    def add_handler(self, handler, path):
-        # Add a handler for a path
-        # You will need to split the path and pass the pass parts
-        # as a list to the RouteTrie
-        path_list = self.split_path(path)
+    def add_handler(self, handler, homepage_path):
+        # Add new handler for homepage path
+        route_list = self.__split_path(homepage_path)
+        self.route.insert(handler, route_list)
 
-        self.route.insert(handler, path_list)
-
-    def lookup(self, path):
-        # lookup path (by parts) and return the associated handler
-        # you can return None if it's not found or
-        # return the "not found" handler if you added one
-        # bonus points if a path works with and without a trailing slash
-        # e.g. /about and /about/ both return the /about handler
+    def lookup(self, homepage_path):
+        # Return handler from trie for given homepage path
         try:
-            path_list = self.split_path(path)
-        except ValueError:
-            return None
-        if path_list[-1] == '':
-            path_list = path_list[:-1] # Remove trailing slash
-        return self.route.find(path_list)
+            route_list = self.__split_path(homepage_path)
+            # Remove trailing slash, if existing
+            if route_list[-1] == '': 
+                route_list = route_list[:-1]  
+            # Get handler from route trie 
+            handler = self.route.find(route_list)
+            return handler
 
-    def split_path(self, path):
-        # Splits complete path from root path and returns path sections
-        path_without_root = path.split(self.route.root.path)
-        if path_without_root[0] == path:
-            raise ValueError("Error: Provided path doesn't match root path.")
-        return path_without_root[1].split('/')
+        except KeyError:
+            return "No handler found."
+
+    def __split_path(self, homepage_path):
+        # Splits homepage path into route sections
+        path_without_root = homepage_path.split(self.route.root.path)
+        if path_without_root[0] == homepage_path:
+            raise KeyError("Provided path doesn't match root path.")
+        route_list = path_without_root[1].split('/')
+        return route_list
 
 
 # Test cases
@@ -95,27 +88,27 @@ class Router:
 nd256_classroom = {}
 nd256_classroom['root'] = "https://classroom.udacity.com/nanodegrees/nd256/"
 
-nd256_classroom['dashboard'] = "https://classroom.udacity.com/nanodegrees/nd256/dashboard/overview"
+nd256_classroom['dashboard_handler()'] = "https://classroom.udacity.com/nanodegrees/nd256/dashboard/overview"
 
-nd256_classroom['syllabus'] = "https://classroom.udacity.com/nanodegrees/nd256/syllabus/core-curriculum"
+nd256_classroom['syllabus_handler()'] = "https://classroom.udacity.com/nanodegrees/nd256/syllabus/core-curriculum"
 
-nd256_classroom['introduction'] = "https://classroom.udacity.com/nanodegrees/nd256/parts/f74fc064-524b-4ee8-8fb1-570b3c31a993"
+nd256_classroom['introduction_handler()'] = "https://classroom.udacity.com/nanodegrees/nd256/parts/f74fc064-524b-4ee8-8fb1-570b3c31a993"
 
-nd256_classroom['data_structures'] = "https://classroom.udacity.com/nanodegrees/nd256/parts/b835ca8d-4269-4ca3-b911-c8ceb9cc0aa0"
+nd256_classroom['data_structures_handler()'] = "https://classroom.udacity.com/nanodegrees/nd256/parts/b835ca8d-4269-4ca3-b911-c8ceb9cc0aa0"
 
-nd256_classroom['basic_algorithms'] = "https://classroom.udacity.com/nanodegrees/nd256/parts/da17de0f-f834-46f8-bb48-ee2705d95dc4"
+nd256_classroom['basic_algorithms_handler()'] = "https://classroom.udacity.com/nanodegrees/nd256/parts/da17de0f-f834-46f8-bb48-ee2705d95dc4"
 
-nd256_classroom['advanced_algorithms'] = "https://classroom.udacity.com/nanodegrees/nd256/parts/94ec3f1b-f3ae-4de4-acbc-ec4b27548116"
+nd256_classroom['advanced_algorithms_handler()'] = "https://classroom.udacity.com/nanodegrees/nd256/parts/94ec3f1b-f3ae-4de4-acbc-ec4b27548116"
 
-nd256_classroom['career_services'] = "https://classroom.udacity.com/nanodegrees/nd256/parts/a9c18090-a4e5-47e0-9387-0edbe5e70743"
+nd256_classroom['career_services_handler()'] = "https://classroom.udacity.com/nanodegrees/nd256/parts/a9c18090-a4e5-47e0-9387-0edbe5e70743"
 
-nd256_classroom['introduction-jupyter-notebooks'] = "https://classroom.udacity.com/nanodegrees/nd256/parts/f74fc064-524b-4ee8-8fb1-570b3c31a993/modules/9bbb9a6d-d848-4153-a2fc-25065ee8d42d/lessons/5b27b2cf-9a9b-4d73-bd55-899a9731c864/concepts/1d3e3599-e99a-487f-9522-2508ff6a58c8"
+nd256_classroom['introduction_jupyter_notebooks_handler()'] = "https://classroom.udacity.com/nanodegrees/nd256/parts/f74fc064-524b-4ee8-8fb1-570b3c31a993/modules/9bbb9a6d-d848-4153-a2fc-25065ee8d42d/lessons/5b27b2cf-9a9b-4d73-bd55-899a9731c864/concepts/1d3e3599-e99a-487f-9522-2508ff6a58c8"
 
-nd256_classroom['introduction-faq'] = "https://classroom.udacity.com/nanodegrees/nd256/parts/f74fc064-524b-4ee8-8fb1-570b3c31a993/modules/35f1ebc2-aa55-4a3c-acd0-79e5009a71fd/lessons/2f7e83a4-b637-4df1-a795-4cbfb5d8cd93/concepts/82499d32-744d-4c12-98b2-52bd3936f4b1"
+nd256_classroom['introduction_faq_handler()'] = "https://classroom.udacity.com/nanodegrees/nd256/parts/f74fc064-524b-4ee8-8fb1-570b3c31a993/modules/35f1ebc2-aa55-4a3c-acd0-79e5009a71fd/lessons/2f7e83a4-b637-4df1-a795-4cbfb5d8cd93/concepts/82499d32-744d-4c12-98b2-52bd3936f4b1"
 
-nd256_classroom['introduction-welcome'] = "https://classroom.udacity.com/nanodegrees/nd256/parts/f74fc064-524b-4ee8-8fb1-570b3c31a993/modules/9bbb9a6d-d848-4153-a2fc-25065ee8d42d/lessons/5b27b2cf-9a9b-4d73-bd55-899a9731c864/concepts/13036b92-b510-4d06-ae36-1358dd974ad4"
+nd256_classroom['introduction_welcome_handler()'] = "https://classroom.udacity.com/nanodegrees/nd256/parts/f74fc064-524b-4ee8-8fb1-570b3c31a993/modules/9bbb9a6d-d848-4153-a2fc-25065ee8d42d/lessons/5b27b2cf-9a9b-4d73-bd55-899a9731c864/concepts/13036b92-b510-4d06-ae36-1358dd974ad4"
 
-nd256_classroom['basic_algorithms-project-route_trie'] = "https://classroom.udacity.com/nanodegrees/nd256/parts/da17de0f-f834-46f8-bb48-ee2705d95dc4/modules/bd252a0b-e9e7-473b-bcc1-bc7e3153568b/lessons/8ec390d0-e99d-44c0-88f9-f8f9faf467fc/concepts/8a8492a5-f76d-4dcc-a07c-3e2a2ca9b3c6"
+nd256_classroom['basic_algorithms_project_route_trie_handler()'] = "https://classroom.udacity.com/nanodegrees/nd256/parts/da17de0f-f834-46f8-bb48-ee2705d95dc4/modules/bd252a0b-e9e7-473b-bcc1-bc7e3153568b/lessons/8ec390d0-e99d-44c0-88f9-f8f9faf467fc/concepts/8a8492a5-f76d-4dcc-a07c-3e2a2ca9b3c6"
 
 
 class TestTrieRouter(unittest.TestCase):
@@ -144,7 +137,7 @@ class TestTrieRouter(unittest.TestCase):
     def test_invalid_lookup(cls):
         print("\n##### Test case 3: lookup of non-existing handlers #####")
         lookup_paths = ["https://www.udemy.com/", "https://classroom.udacity.com/nanodegrees/nd256/final_certificate"]
-        expected_handler = [None, None]
+        expected_handler = ["No handler found.", "No handler found."]
         for index, path in enumerate(lookup_paths):
             cls.run_test(path, expected_handler[index])
 
@@ -156,18 +149,3 @@ class TestTrieRouter(unittest.TestCase):
         print("Returned handler: {}".format(actual_handler))
         cls.assertEqual(actual_handler, expected_handler)
 
-'''
-
-# Here are some test cases and expected outputs you can use to test your implementation
-
-# create the router and add a route
-router = Router("root handler", "not found handler") # remove the 'not found handler' if you did not implement this
-router.add_handler("/home/about", "about handler")  # add a route
-
-# some lookups with the expected output
-print(router.lookup("/")) # should print 'root handler'
-print(router.lookup("/home")) # should print 'not found handler' or None if you did not implement one
-print(router.lookup("/home/about")) # should print 'about handler'
-print(router.lookup("/home/about/")) # should print 'about handler' or None if you did not handle trailing slashes
-print(router.lookup("/home/about/me")) # should print 'not found handler' or None if you did not implement one
-'''
